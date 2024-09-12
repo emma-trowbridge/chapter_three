7a5346d89692dd6c3687edd8630553ec1042c639
emmat <emma.trowbridgee@gmail.com>
2024-09-12 16:27:15 -0500

    Commit V2

---
+++ app/src/main/java/com/bignerdranch/android/chapter_two_interactiveui/Question.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.chapter_two_interactiveui
+
+import androidx.annotation.StringRes
+
+data class Question (@StringRes val textResId:Int, val answer: Boolean) //class that has two properties
+
+
